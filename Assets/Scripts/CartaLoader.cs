afd69392f35c7d9d0872d06f67c9f80f42fbeddf
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2023-04-12 09:58:22 -0600

    Pruebas Eventos Oyentes y reorganizacion

--- Assets/Scripts/CartaLoader.cs
+++ Assets/Scripts/CartaLoader.cs
@@ -7,30 +7,26 @@ using UnityEngine.Assertions;
 public class CartaLoader : MonoBehaviour
 {
     [SerializeField] private CartaSO _datos;
-
     [SerializeField] private TMP_Text _topText;
-    
     [SerializeField] private TMP_Text _bottomText; 
-
     [SerializeField] private SpriteRenderer _spriteRenderer;
-    void Start()
-    {
+    void Start() {
         Assert.IsNotNull(_datos, "NO HAY DATOS DEL PREFAB CARTA NO PUEDEN SER NULOS");
+        Randomize();
+    }
+
+    /*
+    public void EscucharBoton(){ 
+    }
+    */
+
+    public void Randomize() {
         _topText.text = _datos._nombre;
         _bottomText.text = _datos._ataque;
         _spriteRenderer.sprite = _datos._sprite;
     }
 
-    public void Randomize()
-    {
-
-        _topText.text = _datos._nombre;
-        _bottomText.text = _datos._ataque;
-        _spriteRenderer.sprite = _datos._sprite;
-    }
-
-    public void SetData(CartaSO datos)
-    {
+    public void SetData(CartaSO datos) {
         _datos = datos;
     }
     
--- Assets/Scripts/DeckLoader.cs
+++ Assets/Scripts/DeckLoader.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class DeckLoader : MonoBehaviour
 {
-    [SerializeField]
-    private CartaLoader _cartaPrefab;
-    
-    [SerializeField] 
-    private CartaSO[] _cartas;
-    
-    [SerializeField]
-    private GameObject _carta;
+    [SerializeField] private CartaLoader _cartaPrefab;
+    [SerializeField] private CartaSO[] _cartas;
+    [SerializeField] private GameObject _carta;
 
     float x = -10.5f;
 
@@ -20,8 +15,7 @@ public class DeckLoader : MonoBehaviour
         Randomize();
     }
 
-    public void Randomize()
-    {
+    public void Randomize() {
         for (int i = 0; i < 5 ; i++) {
             x = x + 3.5f;
             Vector3 pos = new Vector3(x, 0, 0);
@@ -38,5 +32,10 @@ public class DeckLoader : MonoBehaviour
         }
         Randomize();
     }
+
+    // Fisher Yates Shuffle method to randomize the array without repeating elements 
     
+    // Resources.Load to load the data from the resources folder in real time; its versatile and easy to use 
+    // but is not the best option for performance
+
 }
